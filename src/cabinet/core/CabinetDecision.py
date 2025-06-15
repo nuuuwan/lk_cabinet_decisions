82f40ba6aa8ae7baeef6774690bf7949c670a875
nuuuwan <nuuuwan@gmail.com>
2025-06-15 10:14:37 +0530

    to_dict

--- src/cabinet/core/CabinetDecision.py
+++ src/cabinet/core/CabinetDecision.py
@@ -6,3 +6,10 @@ class CabinetDecision:
     decision_num: int
     title: str
     decision_details: str
+
+    def to_dict(self):
+        return {
+            "decision_num": self.decision_num,
+            "title": self.title,
+            "decision_details": self.decision_details,
+        }
