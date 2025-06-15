57bfdb13d2fa92233e61ac1f17ca25904bb06ff8
nuuuwan <nuuuwan@gmail.com>
2025-06-15 16:59:49 +0530

    Pipeline(do_shuffle=False) in tests

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -7,7 +7,7 @@ from cabinet import CabinetDecision, Pipeline
 class TestCase(unittest.TestCase):
 
     def test_run(self):
-        pipeline = Pipeline()
+        pipeline = Pipeline(do_shuffle=False)
         pipeline.run(max_n_hot=0)
         self.assertTrue(
             os.path.exists(CabinetDecision.CABINET_DESICIONS_TABLE_PATH)
