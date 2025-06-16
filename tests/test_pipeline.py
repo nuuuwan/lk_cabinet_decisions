21e3fd056aab3faebe9000523145c1b689f47d17
nuuuwan <nuuuwan@gmail.com>
2025-06-16 08:33:03 +0530

    Do shuffle. Test with max_n_hot > 0

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -7,8 +7,8 @@ from cabinet import CabinetDecision, Pipeline
 class TestCase(unittest.TestCase):
 
     def test_run(self):
-        pipeline = Pipeline(do_shuffle=False)
-        pipeline.run(max_n_hot=0)
+        pipeline = Pipeline(do_shuffle=True)
+        pipeline.run(max_n_hot=1)
         self.assertTrue(
             os.path.exists(CabinetDecision.CABINET_DESICIONS_TABLE_PATH)
         )
