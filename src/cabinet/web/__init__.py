e01817797434e78848369f4d6162ef6d9252a972
nuuuwan <nuuuwan@gmail.com>
2025-09-20 07:54:02 +0530

    deleted year

--- src/cabinet/web/__init__.py
+++
@@ -1,8 +0,0 @@
-# cabinet.web (auto generate by build_inits.py)
-# flake8: noqa: F408
-
-from cabinet.web.CabinetWebPage import CabinetWebPage
-from cabinet.web.ContentsPage import ContentsPage
-from cabinet.web.DayPage import DayPage
-from cabinet.web.DecisionDetailsPage import DecisionDetailsPage
-from cabinet.web.YearPage import YearPage
