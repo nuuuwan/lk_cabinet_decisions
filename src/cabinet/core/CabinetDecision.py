d51748c7e2fc714ba4c42147dfad82033c33b28e
nuuuwan <nuuuwan@gmail.com>
2025-09-20 08:33:38 +0530

    various fixes to CabinetDecisionsWebMixin

--- src/cabinet/core/CabinetDecision.py
+++ src/cabinet/core/CabinetDecision.py
@@ -1,3 +1,4 @@
+import re
 from dataclasses import dataclass
 from functools import cached_property
 from typing import Generator
@@ -11,6 +12,14 @@ log = Log("CabinetDecision")
 
 
 class CabinetDecisionsWebMixin:
+
+    URL_BASE = "https://www.cabinetoffice.gov.lk"
+    URL_DECISIONS = (
+        URL_BASE
+        + "/cab/index.php"
+        + "?option=com_content&view=article&id=63&Itemid=43&lang=en"
+    )
+
     @classmethod
     def get_doc_from_url_details(
         cls, num, date_str, description, url_details
@@ -19,10 +28,10 @@ class CabinetDecisionsWebMixin:
         soup = www_details.soup
         if not soup:
             return None
-        div_title = soup.find("div", class_="cab_heading_text_e")
+        div_title = soup.find("div", id="cab_heading_text_e")
         title_text = div_title.text.strip()
         assert date_str in title_text
-        div_body = soup.find("div", class_="cab_normal_text_e")
+        div_body = soup.find("div", id="cab_normal_text_e")
         decision_details = div_body.text.strip()
         return CabinetDecision(
             num=num,
@@ -33,25 +42,29 @@ class CabinetDecisionsWebMixin:
             decision_details=decision_details,
         )
 
+    @staticmethod
+    def __clean_url__(url):
+        url = re.sub(r"\s+", "", url)
+        return url
+
     @classmethod
     def gen_docs_from_url_date(
         cls, date_str, url_date
     ) -> Generator["CabinetDecision", None, None]:
         www_date = WWW(url_date)
         soup = www_date.soup
-        if not soup:
-            return
-        tables = soup.find_all("table")
-        if len(tables) < 2:
-            return
-        for tr in tables[1].find_all("tr"):
+        assert soup
+        tables = soup.find_all("table", attrs={"width": "95%"})
+        assert len(tables) == 3
+        table = tables[1]
+        for tr in table.find_all("tr"):
             tds = tr.find_all("td")
-            assert len(tds) >= 2
+            assert len(tds) == 2
             num = tds[0].text.strip()
             a = tds[1].find("a")
             description = a.text.strip()
-            url_details = cls.URL_BASE + a["href"]
-
+            url_details = f'{cls.URL_BASE}/cab/{a["href"]}'
+            url_details = cls.__clean_url__(url_details)
             doc = cls.get_doc_from_url_details(
                 num, date_str, description, url_details
             )
@@ -64,45 +77,40 @@ class CabinetDecisionsWebMixin:
     ) -> Generator[tuple[str, str], None, None]:
         www_year = WWW(url_year)
         soup = www_year.soup
-        if not soup:
-            return
-        table = soup.find("table")
+        assert soup
+        tables = soup.find_all("table", attrs={"width": "85%"})
+        assert len(tables) == 2
+        table = tables[1]
         for tr in table.find_all("tr"):
             for td in tr.find_all("td"):
                 a = td.find("a")
                 date_str = a.text.strip()
                 assert (
                     len(date_str) == 10
-                    and date_str[2] == "-"
-                    and date_str[5] == "-"
-                )
+                    and date_str[4] == "-"
+                    and date_str[7] == "-"
+                ), date_str
                 assert date_str[:4] == year_str
-                url_date = cls.URL_BASE + a["href"]
+                url_date = f'{cls.URL_BASE}/cab/{a["href"]}'
                 yield date_str, url_date
 
     @classmethod
     def gen_urls_years(cls) -> Generator[tuple[str, str], None, None]:
         www_home = WWW(cls.URL_DECISIONS)
         soup = www_home.soup
-        if not soup:
-            return
+        assert soup
         ul = soup.find("ul", class_="menu")
         for li in ul.find_all("li"):
             a = li.find("a")
             year_str = a.text.strip()
             assert len(year_str) == 4 and year_str.isdigit()
-            url_year = cls.URL_BASE + a["href"]
+            url_year = f'{cls.URL_BASE}/{a["href"]}'
             yield year_str, url_year
 
 
 @dataclass
 class CabinetDecision(AbstractDoc, CabinetDecisionsWebMixin):
     decision_details: str
-
-    URL_BASE = "https://www.cabinetoffice.gov.lk/cab/index.php"
-    URL_DECISIONS = (
-        URL_BASE + "?option=com_content&view=article&id=63&Itemid=43&lang=en"
-    )
 
     @classmethod
     def get_doc_class_description(cls) -> str:
