bad8ea5b1c942e0903e27ad1ef09bf3dac17f73e
nuuuwan <nuuuwan@gmail.com>
2025-09-20 08:16:35 +0530

    updated pipeline

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -1,16 +1,4 @@
+from cabinet import CabinetDecision
+
 if __name__ == "__main__":
-    import sys
-
-    from utils import Log
-
-    from cabinet import Pipeline
-
-    log = Log("Pipeline")
-
-    if len(sys.argv) != 2:
-        raise ValueError("python pipeline.py <max_n_hot>")
-
-    max_n_hot = int(sys.argv[1])
-    log.info(f"{max_n_hot=}")
-    pipeline = Pipeline(do_shuffle=False)
-    pipeline.run(max_n_hot)
+    CabinetDecision.run_pipeline()
