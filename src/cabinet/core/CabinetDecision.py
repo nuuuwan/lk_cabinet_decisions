7631e603ccca550008445fa8113d73126708cbb7
nuuuwan <nuuuwan@gmail.com>
2025-09-20 08:58:41 +0530

    Updated doc_id

--- src/cabinet/core/CabinetDecision.py
+++ src/cabinet/core/CabinetDecision.py
@@ -15,6 +15,15 @@ class CabinetDecision(AbstractDoc, CabinetDecisionsWebMixin):
     decision_details_title: str
     decision_details_body: str
 
+    @cached_property
+    def num_int(self):
+        return int(self.num)
+
+    @cached_property
+    def doc_id(self):
+        doc_id = f"{self.date_str}-{self.num_int:03d}-{self.lang}"
+        return doc_id
+
     @classmethod
     def get_doc_class_description(cls) -> str:
         return "A Sri Lanka Cabinet Decision is an official policy or action agreed by the Cabinet of Ministers, shaping governance, law, and national development in the country."  # noqa: E501
