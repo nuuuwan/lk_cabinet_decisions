e6b2657e2e70f7c4aeb516ea5f99d4bccd9370dd
nuuuwan <nuuuwan@gmail.com>
2025-09-20 11:06:23 +0530

    changed shuffle logic

--- src/cabinet/core/CabinetDecisionsWebMixin.py
+++ src/cabinet/core/CabinetDecisionsWebMixin.py
@@ -114,7 +114,7 @@ class CabinetDecisionsWebMixin:
             return
         ul = soup.find("ul", class_="menu")
         lis = ul.find_all("li")
-        random.shuffle(lis)
+        lis = [lis[0]] + random.sample(lis[1:], len(lis) - 1)
         for li in lis:
             a = li.find("a")
             year_str = a.text.strip()
