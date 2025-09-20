cd46dd9b460ba46da126757fa8795196b002d563
nuuuwan <nuuuwan@gmail.com>
2025-09-20 07:52:18 +0530

    deleted pipeline

--- src/cabinet/pipeline/__init__.py
+++
@@ -1,6 +0,0 @@
-# cabinet.pipeline (auto generate by build_inits.py)
-# flake8: noqa: F408
-
-from cabinet.pipeline.CabinetDecisionChart import CabinetDecisionChart
-from cabinet.pipeline.Pipeline import Pipeline
-from cabinet.pipeline.ReadMe import ReadMe
